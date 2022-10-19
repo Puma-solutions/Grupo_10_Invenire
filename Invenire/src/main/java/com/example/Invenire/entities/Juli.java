26de48cf94a71748e9adababaf4bd68dbbfe64d2
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-10-19 12:07:01 -0300

    Eliminacion clases de prueba

--- Invenire/src/main/java/com/example/Invenire/entities/Juli.java
+++
@@ -1,6 +0,0 @@
-package com.example.Invenire.entities;
-
-public class Juli {
-    private String hola;
-}
-
--- Invenire/src/main/java/com/example/Invenire/entities/franco.java
+++
@@ -1,5 +0,0 @@
-package com.example.Invenire.entities;
-
-public class franco {
-    private String franco;
-}
