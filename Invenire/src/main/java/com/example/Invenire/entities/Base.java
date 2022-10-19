66c4fcda200fee696f362ab3920655c809d70b31
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-10-19 11:11:02 -0300

    Proyecto inicial

---
+++ Invenire/src/main/java/com/example/Invenire/entities/Base.java
@@ -0,0 +1,24 @@
+package com.example.Invenire.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Base implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+}
