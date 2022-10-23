009a200a8eaed5fa7ec0dc6f25504558c642315e
CharlyMoreno <carlitosmoreno12@hotmail.com>
2022-10-23 17:48:08 -0300

    Repositorios

---
+++ Invenire/src/main/java/com/example/Invenire/repositories/CursoRepository.java
@@ -0,0 +1,8 @@
+package com.example.Invenire.repositories;
+
+import com.example.Invenire.entities.Curso;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CursoRepository extends BaseRepository<Curso,Long>{
+}
---
+++ Invenire/src/main/java/com/example/Invenire/repositories/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.Invenire.repositories;
+
+import com.example.Invenire.entities.Usuario;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends BaseRepository<Usuario,Long>{
+}
